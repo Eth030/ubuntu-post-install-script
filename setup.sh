8d9bb2397fb7bef5c33ead3c6062dec03174f52a
Eth0 <berlin30@me.com>
2024-03-29 16:59:44 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,70 +18,75 @@ create_user() {
     if id "$username" &>/dev/null; then
         echo "User $username already exists."
     else
-        adduser --disabled-password --gecos "" "$username"
+        adduser "$username"
         usermod -aG sudo "$username"
         echo "$username has been created and added to the sudo group."
     fi
 }
 
-# Function to install Cockpit
-install_cockpit() {
-    if ! dpkg -l | grep -qw cockpit; then
-        echo "Installing Cockpit..."
-        apt-get install cockpit -y
-        systemctl enable --now cockpit.socket
-        echo "Cockpit installation completed. Access it via http://your_server_ip:9090."
-    else
-        echo "Cockpit is already installed."
-    fi
+# Function to install and enable firewall
+setup_firewall() {
+    echo "Setting up the firewall..."
+    ufw enable
+    ufw default deny incoming
+    ufw default allow outgoing
+    echo "Firewall is configured and enabled."
 }
 
-# Function to install Docker
-install_docker() {
-    if ! type docker &>/dev/null; then
-        echo "Installing Docker..."
+# Function to enhance SSH security
+secure_ssh() {
+    echo "Securing SSH..."
+    # Additional security measures can be added here as needed
+    systemctl reload sshd
+    echo "SSH has been secured."
+}
+
+# Function to install fail2ban
+install_fail2ban() {
+    echo "Installing and configuring fail2ban..."
+    apt-get install fail2ban -y
+    systemctl start fail2ban
+    systemctl enable fail2ban
+    echo "fail2ban is installed and running."
+}
+
+# Function to install optional software
+install_optional_software() {
+    read -p "Do you want to install Cockpit? (yes/no) " choice
+    if [[ "$choice" == "yes" ]]; then
+        apt-get install cockpit -y
+        systemctl enable --now cockpit.socket
+        echo "Cockpit installed."
+    fi
+    
+    read -p "Do you want to install Docker? (yes/no) " choice
+    if [[ "$choice" == "yes" ]]; then
         apt-get install apt-transport-https ca-certificates curl software-properties-common -y
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
         add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
         apt-get update
         apt-get install docker-ce -y
         systemctl start docker
         systemctl enable docker
-        echo "Docker installation completed."
-    else
-        echo "Docker is already installed."
+        echo "Docker installed."
     fi
-}
-
-# Function to deploy Portainer
-deploy_portainer() {
-    if ! docker ps -a | grep -qw portainer; then
-        echo "Deploying Portainer..."
+    
+    read -p "Do you want to deploy Portainer? (yes/no) " choice
+    if [[ "$choice" == "yes" ]]; then
         docker volume create portainer_data
         docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always \
             -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
-        echo "Portainer deployed. Access it via http://your_server_ip:9000."
-    else
-        echo "Portainer is already deployed."
+        echo "Portainer deployed."
     fi
-}
-
-# Function to setup SSH Public Key Authentication
-setup_ssh() {
-    echo "Configuring SSH for enhanced security..."
-    sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-    sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
-    systemctl restart sshd
-    echo "SSH has been configured to disallow root login and require public key authentication."
 }
 
 # Function to capture and set up the user's SSH public key
 configure_ssh_key() {
-    mkdir -p /home/"$username"/.ssh
-    chmod 700 /home/"$username"/.ssh
     echo "Please paste the public SSH key for $username:"
     read -r sshKey
+    mkdir -p /home/"$username"/.ssh
     echo "$sshKey" > /home/"$username"/.ssh/authorized_keys
+    chmod 700 /home/"$username"/.ssh
     chmod 600 /home/"$username"/.ssh/authorized_keys
     chown -R "$username":"$username" /home/"$username"/.ssh
     echo "SSH key for $username has been configured."
@@ -91,9 +96,9 @@ configure_ssh_key() {
 echo "Beginning the setup process..."
 update_system
 create_user
-install_cockpit
-install_docker
-deploy_portainer
-setup_ssh
+setup_firewall
+secure_ssh
+install_fail2ban
+install_optional_software
 configure_ssh_key
 echo "Setup process completed. Please verify all installations and configurations."
